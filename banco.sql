106edc7c964f6953a10787da2418920e971393a6
Demitrius Belai <demitrius.belai@unesp.br>
2019-08-06 09:51:11 -0300

    Corrige banco de dados

--- banco.sql
+++ banco.sql
@@ -21,17 +21,17 @@ ALTER TABLE questao ADD FOREIGN KEY (id_resposta_correta) REFERENCES resposta(id
 CREATE TABLE usuario_questionario (
     id INTEGER NOT NULL AUTO_INCREMENT,
     usuario TEXT NOT NULL,
-    hora_inicio TIMESTAMP NOT NULL,
-    hora_termino TIMESTAMP,
+    hora_inicio DATETIME NOT NULL,
+    hora_termino DATETIME,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE usuario_questao (
     id_usuario_questionario INTEGER NOT NULL,
     id_questao INTEGER NOT NULL,
-    hora_inicio TIMESTAMP NOT NULL,
-    hora_termino TIMESTAMP,
-    PRIMARY KEY (id_usuario_questionario, id_questao)
+    hora_inicio DATETIME NOT NULL,
+    hora_termino DATETIME,
+    PRIMARY KEY (id_usuario_questionario, id_questao),
     FOREIGN KEY (id_usuario_questionario) REFERENCES usuario_questionario(id),
     FOREIGN KEY (id_questao) REFERENCES questao(id)
 );
@@ -40,9 +40,9 @@ CREATE TABLE usuario_questao_resposta (
     id_usuario_questionario INTEGER NOT NULL,
     id_questao INTEGER NOT NULL,
     id_resposta INTEGER NOT NULL,
-    hora TIMESTAMP NOT NULL,
+    hora DATETIME NOT NULL,
     PRIMARY KEY (id_usuario_questionario, id_questao, id_resposta),
     FOREIGN KEY (id_usuario_questionario) REFERENCES usuario_questionario(id),
-    FOREIGN KEY (id_questao) REFERENCES questao(id)
+    FOREIGN KEY (id_questao) REFERENCES questao(id),
     FOREIGN KEY (id_resposta) REFERENCES resposta(id)
 );
